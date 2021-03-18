bfff87716561251f83ea3569d19b7f4a0b0b0a8b
mrDuskhunter <9162026242@list.ru>
2021-03-18 17:56:25 +0300

    reload

--- src/main/java/org/example/testParsingString/ControllerV1.java
+++ src/main/java/org/example/testParsingString/ControllerV1.java
@@ -3,15 +3,17 @@ package org.example.testParsingString;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/wordanalyzer")
 public class ControllerV1 {
-    @GetMapping("/greeting")
+    @GetMapping("/analyze")
     public String controllerV1(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        String result = Parser.parser(name);
-        model.addAttribute("name", result);
-        return "greeting";
+            @RequestParam(name="word", required=false) String word, Model model) {
+        String result = Parser.parser(word);
+        model.addAttribute("result", result);
+        return "analyze";
     }
 }
